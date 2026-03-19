1c69baf8511a10b01693a51df5592f3f1e0c979b
resav00 <maciej.ja2004@gmail.com>
2026-03-19 20:14:16 +0100

    Wstępny wybór pracy dyplomowej przez studenta

--- Data/Migrations/20260319191126_AddStudentReservationToTopics.Designer.cs
+++ Data/Migrations/20260319191126_AddStudentReservationToTopics.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using szpont.Data;
 
@@ -10,9 +11,11 @@ using szpont.Data;
 namespace szpont.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260319191126_AddStudentReservationToTopics")]
+    partial class AddStudentReservationToTopics
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.19");
---
+++ Data/Migrations/20260319191126_AddStudentReservationToTopics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace szpont.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentReservationToTopics : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReservationDate",
+                table: "Topics",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentId",
+                table: "Topics",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Topics_StudentId",
+                table: "Topics",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Topics_AspNetUsers_StudentId",
+                table: "Topics",
+                column: "StudentId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Topics_AspNetUsers_StudentId",
+                table: "Topics");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Topics_StudentId",
+                table: "Topics");
+
+            migrationBuilder.DropColumn(
+                name: "ReservationDate",
+                table: "Topics");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Topics");
+        }
+    }
+}
